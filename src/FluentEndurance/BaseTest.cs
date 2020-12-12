b0b3e56d0b5cd43fbc8d5bc0620e1d568a04d437
gcastellov <dreeyfus20@hotmail.com>
2020-12-12 22:09:13 +0100

    adding basetest class

---
+++ src/FluentEndurance/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FluentEndurance
+{
+    public abstract class BaseTest
+    {
+        private readonly IHost _host;
+
+        protected BaseTest(Action<IServiceCollection> configureServices)
+        {
+            var hostBuilder = new HostBuilder();
+
+            hostBuilder.ConfigureServices(collection =>
+            {
+                collection.AddScoped<FeatureSetGroup>();
+                collection.AddSingleton<IMediator>(sp => new Mediator(sp.GetService));
+                configureServices(collection);
+            });
+
+            _host = hostBuilder.Build();
+        }
+
+        protected IHost Host => _host;
+
+        protected FeatureSetGroup UseFeatureSetGroup(Times times)
+        {
+            return _host.Services.GetService<FeatureSetGroup>().For(times);
+        }
+    }
+}
