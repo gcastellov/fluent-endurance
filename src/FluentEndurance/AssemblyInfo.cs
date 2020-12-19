9ab25d168fb3e6609efab27ad3c30eb021e98c91
gcastellov <dreeyfus20@hotmail.com>
2020-12-19 20:21:17 +0100

    fixing Time values

---
+++ src/FluentEndurance/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("790d305c-3bf7-47c8-b5fe-a672393825f0")]
+[assembly: InternalsVisibleTo("FluentEndurance.Tests")]
